528c91d2cdf2841ac58ee6346d28f6565e9ccc1d
SonPham <phamquangson1994@gmail.com>
2023-10-19 08:02:53 +0700

    Add the AddTracing extension method

--- src/Play.Catalog.Service/Startup.cs
+++ src/Play.Catalog.Service/Startup.cs
@@ -10,6 +10,7 @@ using Play.Common.Identity;
 using Play.Common.Logging;
 using Play.Common.MassTransit;
 using Play.Common.MongoDB;
+using Play.Common.OpenTelemetry;
 using Play.Common.Settings;
 
 namespace Play.Catalog.Service
@@ -65,7 +66,8 @@ namespace Play.Catalog.Service
             services.AddHealthChecks()
                     .AddMongoDb();
 
-            services.AddSeqLogging(Configuration);
+            services.AddSeqLogging(Configuration)
+                .AddTracing(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
